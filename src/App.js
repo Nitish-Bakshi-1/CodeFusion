09300dfef183edf8cee42af2e9bd20bd126008dc
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-07 23:40:59 +0530

    home-page done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Home from "./pages/Home.js";
+import EditorPage from "./pages/EditorPage.js";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor/:roomId" element={<EditorPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        exit={{ y: 100, opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="logo">codefusion</div>
+        <h4>Paste invitaion ROOM ID</h4>
+        <div className="input-container">
+          <input type="text" placeholder="ROOM ID" />
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="btn-div">
+          <button className="btn">click</button>
+        </div>
+        <h5>
+          if you dont have a room <a href="#">CREATE</a>
+        </h5>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Home;
