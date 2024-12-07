86287e1d0b00c97b86e560db7de8e8b60e472628
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-08 01:50:37 +0530

    room id

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import { FaGithub } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
 
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    setRoomId(id);
+  };
   return (
     <div className="home">
       <motion.div
-        initial={{ y: 100, opacity: 0 }}
+        initial={{ y: 80, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
-        exit={{ y: 100, opacity: 0 }}
+        exit={{ y: 80, opacity: 0 }}
         transition={{ duration: 0.5 }}
       >
         <div className="logo">codefusion</div>
-        <h4>Paste invitaion ROOM ID</h4>
+        <h4>Paste invitaion/ ROOM ID</h4>
         <div className="input-container">
-          <input type="text" placeholder="ROOM ID" />
+          <input type="text" placeholder="ROOM ID" value={roomId} />
           <input type="text" placeholder="Username" />
         </div>
         <div className="btn-div">
           <button className="btn">click</button>
         </div>
         <h5>
-          if you dont have a room <a href="#">CREATE</a>
+          in case you dont have a room{" "}
+          <a className="createRoomBtn" onClick={createNewRoom} href="">
+            CREATE
+          </a>
         </h5>
       </motion.div>
+      <h4 className="homeFooter">
+        built by
+        <a
+          href="https://github.com/Nitish-Bakshi-1"
+          alt="github"
+          target="_blank"
+        >
+          <FaGithub className="gitIcon" />
+        </a>
+      </h4>
     </div>
   );
 };
